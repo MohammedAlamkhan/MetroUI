7f5e3f36c05c59a55cc8f7dcb2d8d5f9276dbd9a
MohammedAlamKhan <mohammedalamkhan6@gmail.com>
2024-02-22 13:19:49 +0530

    added launch app logic

--- build/js/appList.js
+++ build/js/appList.js
@@ -1,8 +1,8 @@
 // Create a script.js file
 
 // Define a function that will run as soon as the HTML page loads
-var applist
-window.onload = function() {
+var applist;
+document.addEventListener("DOMContentLoaded", function() {
     // Your code here
 
     fetch(Bridge.getAppsURL())
@@ -12,37 +12,16 @@ window.onload = function() {
         applist= resp.apps 
         Bridge.showToast(JSON.stringify(resp.apps));
     })
-    console.log('The HTML page has finished loading.');
+
     // You can write any JavaScript code here that you want to run when the page loads.
 
-
-    var list = document.getElementById("mainAppList");
-
-    // Iterate over the array and create list items
-    applist.forEach(function(item) {
-        // Create a new list item element
-        var li = document.createElement("li");
-
-        li.classList.add("custom-app-item");
-        // Set the text content of the list item to the current item in the array
-        li.textContent = item.label;
-
-        // Attach an event listener to the list item for the click event
-        li.addEventListener("click", function() {
-            // Call the onItemClick function when the list item is clicked, passing the item name
-            launchApp(item.packageName);
-        });
-
-
-        // Append the list item to the list
-        list.appendChild(li);
-    });
-};
+});
 
 function launchApp(packageName){
-    Bridge.requestLaunchApp(packageName)
-  }
+    Bridge.requestLaunchApp(packageName);
+}
 
 function getIcon(packageName){
-    Bridge.getDefaultAppIconURL(packageName)
+    Bridge.getDefaultAppIconURL(packageName);
 }
+s
